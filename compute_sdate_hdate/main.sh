5c7b8078755ffe699d5bd182814f6cb924558c6c
Sara Minoli <sara.minoli@pik-potsdam.de>
2020-07-04 11:53:54 +0200

    Modified main.sh

--- compute_sdate_hdate/main.sh
+++ compute_sdate_hdate/main.sh
@@ -6,10 +6,11 @@
 #SBATCH --job-name=grow_periods
 #SBATCH --account=macmit
 #SBATCH --mail-user=sara.minoli
-#SBATCH --workdir=/p/projects/macmit/users/minoli/PROJECTS/CROP_PHENOLOGY_v01/SCRIPTS/GROWING_PERIODS_PACKAGE/CODE
+#SBATCH --workdir=/home/minoli/crop_calendars_gitlab/crop_phen_paper/compute_sdate_hdate
 #SBATCH --output=out_err/gp_%j_%a.out
 #SBATCH --error=out_err/gp_%j_%a.err
 
+# #SBATCH --array=1-12 # (12 scenarios for preprocessing climate)
 #SBATCH --array=1-72 # 1-72 (12 scenarios * 6 crops)
 
 module load intel/2018.1
@@ -17,6 +18,8 @@ module load intel/2018.1
 module load R/3.4.4
 
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+#R -f ./preprocessing/prepare_monthly_climate.R --args $SLURM_ARRAY_TASK_ID
 
 R -f main.R --args $SLURM_ARRAY_TASK_ID
 wait
