9d0962e8f865b384184c83a06aca155ebbb96bb1
Sara Minoli <minoli@LT863.pik-potsdam.de>
2021-03-15 13:20:49 +0100

    Fixing replace.jumps.R to replace also jumps in the last time slice.

--- compute_sdate_hdate/src/replace.jumps.R
+++ compute_sdate_hdate/src/replace.jumps.R
@@ -13,7 +13,7 @@ replace.jumps <- function(vec.class, vec.date,
   mark.replaced <- vec.date; mark.replaced[] <- 0L
   
   # when is there a change in vec.class?
-  x  <- which(diff(vec.class)!=0)+1 # +1 bcz diff gives index previous value
+  x  <- c(which(diff(vec.class)!=0)+1, length(vec.class)) # +1 bcz diff gives index previous value
   # which changes are temporary (jumps), e.g. less 20 y?
   y1 <- x[which(diff(x)<=min.period)]   # start of temporary change
   y2 <- x[which(diff(x)<=min.period)+1] # end of temporary change
@@ -22,13 +22,13 @@ replace.jumps <- function(vec.class, vec.date,
   if (length(y1)>0)
     
     # loop through actual jumps only (odd indices), skip returns to "normal" value
-    for (i in which( (1:length(y1))%%2==1 ) ) { # x%%2==1 finds odd nr.
+    for (i in c( which( (1:(length(y1)-1))%%2==1), length(y1) ) ) { # x%%2==1 finds odd nr.
       
       vprev <- vec.date.out[y1[i]-1] # value of previous time slice
       vnext <- vec.date.out[y2[i]+1] # value next time slice
       
       if (replacing.value=="mean") {
-        vec.date.out[y1[i]:(y2[i]-1)] <- round(mean(c(vprev, vnext)))
+        vec.date.out[y1[i]:(y2[i]-1)] <- round(mean(c(vprev, vnext), na.rm = T))
       } else if (replacing.value=="previous") {
         vec.date.out[y1[i]:(y2[i]-1)] <- vprev
       } else if (replacing.value=="next") {
@@ -37,6 +37,13 @@ replace.jumps <- function(vec.class, vec.date,
       
       # Mark value to flag values that have been replaced
       mark.replaced[y1[i]:(y2[i]-1)] <- mark.value
+      
+      if (i == length(y1)) {
+        vec.date.out[y2[i]] <- vec.date.out[(y2[i]-1)] # replace last value
+        mark.replaced[y2[i]] <- mark.replaced[(y2[i]-1)] # replace last value
+      }
+      
+      
     }
   if (!marking) return(vec.date.out) else return(mark.replaced)
 }
